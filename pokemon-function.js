3b91af81dd5207c845172b4435658fd11ceb8142
AlphaKretin <deewhydeeecks@gmail.com>
2016-11-14 21:51:04 +1030

    Update pokemon-function.js

--- pokemon-function.js
+++ pokemon-function.js
@@ -14,6 +14,24 @@ function pokemon(user, userID, channelID, message, event){
 				message: "Image: " + ivysaur.image + "\nName: " + ivysaur.name + "\nPokédex No.: " + ivysaur.dex + "\nType: " + ivysaur.type + "\nAbility: " + ivysaur.ability + "\nEvolution: " + ivysaur.evolve + "\nWiki Link: " + ivysaur.wiki
 			});
 			break;
+		case "venusaur":
+			bot.sendMessage({
+				to: channelID,
+				message: "Image: " + venusaur.image + "\nName: " + venusaur.name + "\nPokédex No.: " + venusaur.dex + "\nType: " + venusaur.type + "\nAbility: " + venusaur.ability + "\nEvolution: " + venusaur.evolve + "\nWiki Link: " + venusaur.wiki
+			});
+			break;
+		case "charmander":
+			bot.sendMessage({
+				to: channelID,
+				message: "Image: " + charmander.image + "\nName: " + charmander.name + "\nPokédex No.: " + charmander.dex + "\nType: " + charmander.type + "\nAbility: " + charmander.ability + "\nEvolution: " + charmander.evolve + "\nWiki Link: " + charmander.wiki
+			});
+			break;
+		case "charmeleon":
+			bot.sendMessage({
+				to: channelID,
+				message: "Image: " + charmeleon.image + "\nName: " + charmeleon.name + "\nPokédex No.: " + charmeleon.dex + "\nType: " + charmeleon.type + "\nAbility: " + charmeleon.ability + "\nEvolution: " + charmeleon.evolve + "\nWiki Link: " + charmeleon.wiki
+			});
+			break;
 		default:
 			bot.sendMessage({
 				to: channelID,
