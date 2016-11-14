12a8d996bcc45caf44bafde8e893b21450dee6fe
AlphaKretin <deewhydeeecks@gmail.com>
2016-11-14 21:47:41 +1030

    Create snippets.js

---
+++ snippets.js
@@ -0,0 +1,26 @@
+case "butts":
+			bot.sendMessage({
+				to: channelID,
+				message: "Image: " + butts.image + "\nName: " + butts.name + "\nPokédex No.: " + butts.dex + "\nType: " + butts.type + "\nAbility: " + butts.ability + "\nEvolution: " + butts.evolve + "\nWiki Link: " + butts.wiki
+			});
+			break;
+			
+var bulbasaur = {
+	name: "Bulbasaur",
+	dex: 1,
+	type: "Grass/Poison",
+	ability: "Overgrow, N/A, Chlorophyll",
+	evolve: "Level 16, Ivysaur",
+	wiki: "http://bulbapedia.bulbagarden.net/wiki/Bulbasaur_(Pok%C3%A9mon)",
+	image: "http://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/240px-001Bulbasaur.png"
+}
+
+var ivysaur = {
+	name: "Ivysaur",
+	dex: 2,
+	type: "Grass/Poison",
+	ability: "Overgrow, N/A, Chlorophyll",
+	evolve: "Level 32, Venusaur",
+	wiki: "http://bulbapedia.bulbagarden.net/wiki/Ivysaur_(Pok%C3%A9mon)",
+	image: "http://cdn.bulbagarden.net/upload/thumb/7/73/002Ivysaur.png/240px-002Ivysaur.png"
+}
